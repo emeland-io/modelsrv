6eb8278cd8723c9c4b7b24469191573461197fa9
XHPJ <xheladinpopaj@outlook.de>
2026-03-31 11:34:00 +0200

    refactor(gen): move code generator to tools/gen and register event handlers
    - Move pkg/model/gen → tools/gen
    - Fix baseDir path in generator (../../pkg/model)
    - Update //go:generate directive in pkg/model/structure.go
    - Replace switch-based event applier with a generated handler registry
    
    refactor(gen): move code generator from pkg/model/gen to tools/gen

--- pkg/model/event_apply.go
+++ pkg/model/event_apply.go
@@ -1,16 +1,11 @@
 package model
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/google/uuid"
 	"go.emeland.io/modelsrv/pkg/events"
-	mdlapi "go.emeland.io/modelsrv/pkg/model/api"
-	"go.emeland.io/modelsrv/pkg/model/common"
-	"go.emeland.io/modelsrv/pkg/model/component"
 	mdlevent "go.emeland.io/modelsrv/pkg/model/event"
-	"go.emeland.io/modelsrv/pkg/model/system"
 )
 
 var _ mdlevent.EventApplier = (*modelData)(nil)
@@ -31,77 +26,21 @@ func (m *modelData) Apply(ev events.Event) error {
 }
 
 func (m *modelData) applyReplicationDelete(rt events.ResourceType, id uuid.UUID) error {
-	var err error
-	switch rt {
-	case events.SystemResource:
-		err = m.DeleteSystemById(id)
-	case events.SystemInstanceResource:
-		err = m.DeleteSystemInstanceById(id)
-	case events.APIResource:
-		err = m.DeleteApiById(id)
-	case events.APIInstanceResource:
-		err = m.DeleteApiInstanceById(id)
-	case events.ComponentResource:
-		err = m.DeleteComponentById(id)
-	case events.ComponentInstanceResource:
-		err = m.DeleteComponentInstanceById(id)
-	default:
+	h, ok := m.handlers[rt]
+	if !ok {
 		return fmt.Errorf("unsupported resource type for delete: %s", rt)
 	}
-	if err != nil && !replicationNotFound(err) {
+	err := h.delete(m, id)
+	if err != nil && !h.notFound(err) {
 		return err
 	}
 	return nil
 }
 
-func replicationNotFound(err error) bool {
-	return errors.Is(err, common.ErrSystemNotFound) ||
-		errors.Is(err, common.ErrSystemInstanceNotFound) ||
-		errors.Is(err, common.ErrApiNotFound) ||
-		errors.Is(err, common.ErrApiInstanceNotFound) ||
-		errors.Is(err, common.ErrComponentNotFound) ||
-		errors.Is(err, common.ErrComponentInstanceNotFound)
-}
-
 func (m *modelData) applyReplicationUpsert(rt events.ResourceType, obj any) error {
-	switch rt {
-	case events.SystemResource:
-		s, ok := obj.(system.System)
-		if !ok {
-			return fmt.Errorf("replication object for System is %T, want System", obj)
-		}
-		return m.AddSystem(s)
-	case events.SystemInstanceResource:
-		s, ok := obj.(system.SystemInstance)
-		if !ok {
-			return fmt.Errorf("replication object for SystemInstance is %T, want SystemInstance", obj)
-		}
-		return m.AddSystemInstance(s)
-	case events.APIResource:
-		a, ok := obj.(mdlapi.API)
-		if !ok {
-			return fmt.Errorf("replication object for API is %T, want API", obj)
-		}
-		return m.AddApi(a)
-	case events.APIInstanceResource:
-		a, ok := obj.(mdlapi.ApiInstance)
-		if !ok {
-			return fmt.Errorf("replication object for ApiInstance is %T, want ApiInstance", obj)
-		}
-		return m.AddApiInstance(a)
-	case events.ComponentResource:
-		c, ok := obj.(component.Component)
-		if !ok {
-			return fmt.Errorf("replication object for Component is %T, want Component", obj)
-		}
-		return m.AddComponent(c)
-	case events.ComponentInstanceResource:
-		c, ok := obj.(component.ComponentInstance)
-		if !ok {
-			return fmt.Errorf("replication object for ComponentInstance is %T, want ComponentInstance", obj)
-		}
-		return m.AddComponentInstance(c)
-	default:
+	h, ok := m.handlers[rt]
+	if !ok {
 		return fmt.Errorf("unsupported resource type for upsert: %s", rt)
 	}
+	return h.upsert(m, obj)
 }
