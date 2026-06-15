9e0efcdcbc2adf8f2ca6587dee67387e2f0d254c
Michael Schnupp <michael.schnupp@gmail.com>
2026-06-15 12:34:15 +0200

    fix: check resp.Body.Close error for errcheck linter

--- cmd/emelandctl/get.go
+++ cmd/emelandctl/get.go
@@ -50,7 +50,7 @@ func fetchResourceList(baseURL, path string) ([]common.InstanceListItem, error) 
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
+	defer func() { _ = resp.Body.Close() }()
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("expected HTTP 200 but received %d", resp.StatusCode)
 	}
